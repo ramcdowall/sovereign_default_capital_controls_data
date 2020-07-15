76940cefa2f34a10c9577f48ad3e729dd047056c
ramcdowall <65321501+ramcdowall@users.noreply.github.com>
2020-07-15 18:35:16 -0400

    updated soln. method, consistent with tbl in paper

--- mcdowall_cert.m
+++ mcdowall_cert.m
@@ -1,3 +1,7 @@
+%% Sovereign Debt and Capital Controls
+% Matlab File 1: certainty case
+% By: Robert A. McDowall
+
 clear all
 clc
 global y_0 y_1 g_0 gamma R tau phi; 
@@ -8,7 +12,6 @@ g_0 = .35;
 beta = .96;
 R = 1.04;
 q = 1/R; 
-tau = 0;
 gamma = 2;
 phi = 0.12; 
 %%
@@ -16,52 +19,38 @@ phi = 0.12;
 util = @(c) (c.^(1-gamma))./(1-gamma);
 mutil = @(c) c.^(-gamma);
 
-%util = @(c) log(c);
-%mutil = @(c) 1./c;
-
-funsolve = @(B_d) -(util(y_0 -q*B_d*(1+tau)) +beta*util(y_1 +B_d - (g_0-q*tau*B_d)/q));
-
-
-%%
-[Bd_max] = fmincon(funsolve,.05, [], [], [], [], 0 ,g_0/R)
 
 %% Comparison - Govt. Spending
 
 g_vec = 0:.01:.6;
 iter = 5;
 B_trans = 0;
-tau = 0; 
+
 
 for i=1:length(g_vec)
     
     g_0 = g_vec(i);
-    
-    for j = 1:iter
-        
-    funsolve = @(B_d) -(util(y_0 -q*B_d*(1+tau)) +beta*util(y_1 +B_d - (g_vec(i)-q*tau*B_trans)/q));
+       
+    funsolve = @(B_d) -(util(y_0 -q*B_d) +beta*util(y_1 +B_d - (g_vec(i)*R)));
     [Bd_max] = fmincon(funsolve,.05, [], [], [], [], 0 ,g_0/R);
-    
-    B_trans = Bd_max;
 
-    end
-    
     Bd_vec(i) = Bd_max;
-    B_vec(i) = (g_0-q*tau*Bd_max)/q;
+    B_vec(i) = g_0*R;
     
     Bf_vec(i) = B_vec(i) - Bd_max;
     
-    c_0 = y_0 - q*Bd_max*(1+tau);
+    c_0 = y_0 - q*Bd_max;
     c_1 = y_1 + Bd_max-B_vec(i);
-    optc_vec(i) = beta*mutil(c_1) - q* mutil(c_0)*(1+tau);
+    optc_vec(i) = beta*mutil(c_1) - q* mutil(c_0);
     
     util_vec(i) = util(c_0) + beta* util(c_1);
     
 end
 
 %%
-
 diff = util(y_1 + Bd_vec-B_vec) - util(y_1 - phi);
 %%
+close all
 plot(g_vec, util_vec)
 
 %% Find Autarchy Values
@@ -81,6 +70,7 @@ for i = 1:length(diff)
     
 end
 %%
+close all
 plot(g_vec(21:end), util_final(21:end))
 hold on 
 plot(g_vec, util_vec, 'green')
@@ -88,70 +78,60 @@ hold on
 plot(g_vec(1:20), util_final(1:20))
 
 %% Opitmal Controls
-
 for i = 1:length(diff)
     
     if diff(i) > 0
         
-        util_CC(i) = util_vec(i)
+        util_CC(i) = util_vec(i);
         
     else
-        
-        %search for fixed point on tau that satisfied IC constraint
-        tau = -0.1; 
-        g_0 = g_vec(i);
-        B_trans = 0.1;
+      
+      %Solve optimal savings implied by optimal tau
+      tau_s = 1 - 1/(( mutil(y_0 - g_vec(i) + phi/R))/(beta*R*mutil(y_1 - phi)));
+      funsolve = @(B_d) -(util(y_0 - q*B_d*(1-tau_s)) +beta*util(y_1 + B_d - (g_vec(i)*R+tau_s*B_d)));
+      
+      [Bd_max] = fmincon(funsolve,.1, [], [], [], [], 0 ,g_vec(i)/R);
+      
+      Bd_CC(i) = Bd_max; 
+      B_CC(i) = (g_vec(i)*R + tau_s*Bd_CC(i));
        
-       for k = 1:iter 
-            for j = 1:iter
+      c_0 = y_0 - q*Bd_CC(i)*(1-tau_s);
+      c_1 = y_1 + Bd_CC(i)-B_CC(i);
         
-                funsolve = @(B_d) -(util(y_0 -q*B_d*(1+tau)) +beta*util(y_1 +B_d - (g_vec(i)-q*tau*B_trans)/q));
-                [Bd_max] = fmincon(funsolve,.1, [], [], [], [], 0 ,g_0/R);
-    
-                 B_trans = Bd_max;
-
-            end
-        
-           Bd_CC(i) = Bd_max;
-            tau_opt = (g_0/q - phi)/Bd_max-1; 
-        
-            tau = tau_opt; 
-       end 
-        
-       tau_optvec(i) = tau_opt;  
-       
-       B_CC(i) = (g_0-q*tau*Bd_max)/q;
-       
-       c_0 = y_0 - q*Bd_max*(1+tau_opt);
-       c_1 = y_1 + Bd_max-B_CC(i);
-        
-       util_CC(i) =  util(c_0) + beta* util(c_1);
+      util_CC(i) =  util(c_0) + beta* util(c_1);
     end
-    
-    
+       
 end
- %%
- close all
+
+%% Plot
+repay_index = max(find(diff>0))
+ 
+close all
 plot(g_vec/y_0, util_vec, 'green', 'LineWidth',2.1)
 hold on
-plot(g_vec(41:end)/y_0, util_final(41:end), '*b', 'LineWidth',1)
+plot(g_vec(repay_index:end)/y_0, util_final(repay_index:end), '*b', 'LineWidth',1)
 axis([.15,.58,-3.2,-1.9])
 hold on 
 plot(g_vec/y_0, util_CC, '--red', 'LineWidth',2.2) 
 hold on
-plot(g_vec(1:41)/y_0, util_vec(1:41), '*b', 'LineWidth',1 )
+plot(g_vec(1:repay_index)/y_0, util_vec(1:repay_index), '*b', 'LineWidth',1 )
 title('Optimal Capital Controls','interpreter','latex','fontsize',18)
 ylabel('Utility','interpreter','latex','fontsize',18)
 xlabel('Government Spending (\% GDP)','interpreter','latex','fontsize',18)
-legend('Commitment', 'No Commitment', 'Capital Controls' ,14,'Location','Northeast')
+legend('Commitment', 'No Commitment', 'Capital Controls')% ,14,'Location','Northeast')
 
 %% Summary Statistics
+clc 
+index = 36;
+g_val = g_vec(index)
 
-Bd_CC(26)/B_CC(26)
-Bd_vec(26)/B_vec(26)
+%Internal debts
+first_best_prop =Bd_vec(index)/B_vec(index)
+ccont_prop = Bd_CC(index)/B_CC(index)
 
-tau_optvec(26)
+%convert from subsidy to tax space
+tau_tbl = ( mutil(y_0 - g_val + phi/R))/(beta*R*mutil(y_1 - phi)) - 1 
 
-
-
-
+%Welfare
+util_vec(index) - util_CC(index)
+util_vec(index) / util_CC(index)
